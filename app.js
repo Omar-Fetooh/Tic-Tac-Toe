3404d8c56edd6263f85828a1ac792a6b23863252
omar fetooh <omarfetooh62@gmail.com>
2024-03-02 21:17:38 +0200

    Almost Working console game

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 function gameBoard() {
     const rows = 3, cols = 3;
-    const board = [];
-
+    const board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
     /*
      should be a 2d array ...
      - - -
      - - -
      - - -
      */
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < cols; j++) {
-            let rnd = Math.random();
-            let char;
-            if (rnd > .5) char = 'x';
-            else char = 'y';
+    let winner = 0;
+    const play = () => {
+        printBoard();
+        currentPlayer = player1;
+        while (winner === 0) {
+            console.log(`it's player ${currentPlayer.name} turn , enter number from 1 to 9 :`);
+            let inp = prompt("Enter your number: ");
 
-            board[i].push(cell(char));
+            inp--;
+            let row = parseInt(inp / 3);
+            let col = inp % 3;
+            if (board[row][col] == 'X' || board[row][col] == 'O') {
+                console.log("You entered a invalid number");
+            }
+            else {
+
+                board[row][col] = currentPlayer.mark;
+                winner = checkFinish();
+                currentPlayer = (currentPlayer === player1) ? player2 : player1;
+                printBoard();
+            }
+        }
+        if (winner === 'D') {
+            console.log("It's a draw ");
+        }
+        else {
+            console.log(`player  ${winner} won the game `);
         }
     }
 
@@ -25,10 +42,7 @@ function gameBoard() {
     // print the board
     const printBoard = () => {
         for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < cols; j++) {
-                console.log(board[i][j], " ")
-                checkFinish();
-            }
+            console.log(board[i]);
         }
     }
 
@@ -38,39 +52,40 @@ function gameBoard() {
         for (let i = 0; i < rows; i++) {
             let j = 0;
             //checks row by row
-            if (board[i][j] === board[i][j + 1] === board[i][j + 2]) {
-                confirm
+            if (board[i][j] === board[i][j + 1] && board[i][j + 1] === board[i][j + 2]) {
                 return board[i][j];
             }
             //checks col by col
-            if (board[j][i] === board[j + 1][i] === board[j + 2][i]) {
+            if (board[j][i] === board[j + 1][i] && board[j + 1][i] === board[j + 2][i]) {
                 return board[i][j];
             }
             // check for main diagonal ...
-            if (board[0][0] === board[1][1] === board[2][2]) {
+            if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
                 return board[0][0];
             }
-            if (board[0][2] === board[1][1] === board[2][0]) {
+            if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
                 return board[0][2];
             }
         }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] != 'X' && board[i][j] != 'O') {
+                    return 0;
+                }
+            }
+        }
+        return 'D';
     }
     return {
         getBoard,
         printBoard,
-        checkFinish
+        checkFinish,
+        play
     }
 }
 
-
-function player(name) {
-    return { name };
+function player(name, mark) {
+    return { name, mark };
 }
-
-
-function cell(char) {
-    // gameBoard();
-    return char;
-}
-
-gameBoard();
+const player1 = player("one", 'X');
+const player2 = player("two", 'O');
